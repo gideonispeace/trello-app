0e80bc843522840a1b571bb2e9b19f7f6bed4a93
gideonispeace <39224847+gideonispeace@users.noreply.github.com>
2020-10-25 01:41:56 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import { createGlobalStyle } from "styled-components";
-import $ from "jquery";
+import $ from "jQuery";
 import { PersistGate } from "redux-persist/integration/react";
 
 const { persistor, store } = Store();
